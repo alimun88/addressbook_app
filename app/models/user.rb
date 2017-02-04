36f8942927e92a3caee04ffd1d3d7c4992f60218
alimun88 <ayon88@hotmail.com>
2017-02-04 17:43:20 +0000

    Added password_digest to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
-    
+  has_many :contacts, dependent: :destroy
+  before_save { self.email = email.downcase }
+  
+  validates :username, presence: true, 
+  uniqueness: {case_sensitive: false}, 
+  length: {minimum: 5, maximum: 25}
+  
+  validates :email, presence: true, length: { maximum: 105 },
+  uniqueness: { case_sensitive: false }
+  validates_format_of :email, :with => /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170203224528) do
+ActiveRecord::Schema.define(version: 20170203225734) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "firstname"
@@ -29,8 +29,9 @@ ActiveRecord::Schema.define(version: 20170203224528) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
