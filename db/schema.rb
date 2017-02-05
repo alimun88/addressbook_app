0cebca882590d0ef64f79422e0ab2066cd287fd3
alimun88 <ayon88@hotmail.com>
2017-02-05 07:20:22 +0000

    Added admin to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170203225734) do
+ActiveRecord::Schema.define(version: 20170205061659) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "firstname"
@@ -29,9 +29,10 @@ ActiveRecord::Schema.define(version: 20170203225734) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
